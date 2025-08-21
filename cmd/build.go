9aea7278f8e1a7da26766f41aad71f646cf2dc2a
Vismai Nair <vismai.nair@gmail.com>
2025-08-20 18:11:07 -0700

    style: add proper newlines to build command
    Add newlines for build command CLI logging.

--- cmd/build.go
+++ cmd/build.go
@@ -1,10 +1,11 @@
 package cmd
 
 import (
-	"os"
-	"os/exec"
+	"fmt"
+	"os"      // For working with the operating syste
+	"os/exec" // For executing cli commands
 
-	"github.com/spf13/cobra"
+	"github.com/spf13/cobra" // Importing the Cobra library for CLI apps
 )
 
 var buildCmd = &cobra.Command{
@@ -12,19 +13,22 @@ var buildCmd = &cobra.Command{
 	Short: "Build Go code to WebAssembly.",
 	Long:  `Build Go code to WebAssembly (WASM) format. /nYou can specify a package or go file to build. If neither are specified, it defaults to all go files in the current directory. /nUnder the hood, it runs the command 'go build GOOS=js GOARCH=wasm'. This command is useful for preparing Go applications to run in a web environment using WebAssembly.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		target := "."
+		target := "." // Default target is current directory
 		if len(args) > 0 {
 			target = args[0] // If a package is specified, use it as target
 		}
 
-		buildCmd := exec.Command("go", "build", target)               // Run the go build command
+		buildCmd := exec.Command("go", "build", target) // Run the go build command
+		fmt.Fprintf(os.Stdout, "Creating WASM Build command... \n")
 		buildCmd.Env = append(os.Environ(), "GOOS=js", "GOARCH=wasm") // Set the environment variables for WebAssembly
-		buildCmd.Stdout = os.Stdout                                   // Redirect standard output to the console
-		buildCmd.Stderr = os.Stderr                                   // Redirect standard error to the console
-		buildCmd.Run()                                                // Execute the build command
+
+		buildCmd.Stdout = os.Stdout // Redirect standard output to the console
+		buildCmd.Stderr = os.Stderr // Redirect standard error to the console
+		fmt.Fprintf(os.Stdout, "Building WASM from provided file or package... \n")
+		buildCmd.Run() // Execute the build command
 	},
 }
 
 func init() {
-	rootCmd.AddCommand(buildCmd)
+	rootCmd.AddCommand(buildCmd) // Add the build command to the root command
 }
